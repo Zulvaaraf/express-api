089236d96429c65b909fa28557fbf86f74145b71
zulvaaraf <arafzulvaa@gmail.com>
2024-01-29 10:49:18 +0700

    init

---
+++ src/controllers/handler.js
@@ -0,0 +1,101 @@
+import { people } from '../data.js';
+
+const postPeopleHandler = (req, res) => {
+  const { id, name } = req.body;
+
+  const personId = { id, name };
+
+  if (!personId) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Gagal menambahkan data person',
+    });
+  }
+
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      personId,
+    },
+  });
+};
+
+const getPeople = (req, res) => {
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      people,
+    },
+  });
+};
+
+const getPeopleById = (req, res) => {
+  const { peopleId } = req.params;
+
+  const peopleID = people.find((people) => people.id === Number(peopleId));
+
+  if (!peopleID) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Gagal! id tidak ditemukan',
+    });
+  }
+
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      peopleID,
+    },
+  });
+};
+
+const editPeopleById = (req, res) => {
+  const { peopleId } = req.params;
+  const { name } = req.body;
+
+  const findPeople = people.find((people) => people.id === Number(peopleId));
+
+  if (!findPeople) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Gagal! id tidak ditemukan',
+    });
+  }
+
+  const newPeople = people.map((people) => {
+    if (people.id === Number(peopleId)) {
+      people.name = name;
+    }
+
+    return people;
+  });
+
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      newPeople,
+    },
+  });
+};
+
+const deletePeopleById = (req, res) => {
+  const { peopleId } = req.params;
+
+  const findIndex = people.findIndex((people) => people.id === Number(peopleId));
+
+  if (findIndex === -1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Gagal! id tidak ditemukan',
+    });
+  } else {
+    people.splice(findIndex, 1);
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Berhasil dihapus',
+    });
+  }
+};
+
+export { postPeopleHandler, getPeople, getPeopleById, editPeopleById, deletePeopleById };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,104 +1,16 @@
 import express from 'express';
-import { people } from '../data.js';
+import { postPeopleHandler, getPeople, getPeopleById, editPeopleById, deletePeopleById } from '../controllers/handler.js';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  const { id, name } = req.body;
+router.post('/', postPeopleHandler);
 
-  const personId = { id, name };
+router.get('/', getPeople);
 
-  if (!personId) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Gagal menambahkan data person',
-    });
-  }
+router.get('/:peopleId', getPeopleById);
 
-  return res.status(200).json({
-    status: 'success',
-    data: {
-      personId,
-    },
-  });
-});
+router.put('/:peopleId', editPeopleById);
 
-router.get('/', (req, res) => {
-  return res.status(200).json({
-    status: 'success',
-    data: {
-      people,
-    },
-  });
-});
-
-router.get('/:peopleId', (req, res) => {
-  const { peopleId } = req.params;
-
-  const peopleID = people.find((people) => people.id === Number(peopleId));
-
-  if (!peopleID) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Gagal! id tidak ditemukan',
-    });
-  }
-
-  return res.status(200).json({
-    status: 'success',
-    data: {
-      peopleID,
-    },
-  });
-});
-
-router.put('/:peopleId', (req, res) => {
-  const { peopleId } = req.params;
-  const { name } = req.body;
-
-  const findPeople = people.find((people) => people.id === Number(peopleId));
-
-  if (!findPeople) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Gagal! id tidak ditemukan',
-    });
-  }
-
-  const newPeople = people.map((people) => {
-    if (people.id === Number(peopleId)) {
-      people.name = name;
-    }
-
-    return people;
-  });
-
-  return res.status(200).json({
-    status: 'fail',
-    data: {
-      newPeople,
-    },
-  });
-});
-
-router.delete('/:peopleId', (req, res) => {
-  const { peopleId } = req.params;
-
-  const findIndex = people.findIndex((people) => people.id === Number(peopleId));
-
-  if (findIndex === -1) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Gagal! id tidak ditemukan',
-    });
-  } else {
-    people.splice(findIndex, 1);
-
-    return res.status(200).json({
-      status: 'success',
-      message: 'Berhasil dihapus',
-    });
-  }
-});
+router.delete('/:peopleId', deletePeopleById);
 
 export default router;
